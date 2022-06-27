bba9222716f8492d35c69b378724f8c2a3a1bbf4
Kewon <48837736+kewon96@users.noreply.github.com>
2022-06-27 23:58:22 +0900

    init project

---
+++ src/main/java/com/thy/springmvc2_thymeleaf/SpringMvc2ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.thy.springmvc2_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc2ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvc2ThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thy/springmvc2_thymeleaf/SpringMvc2ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thy.springmvc2_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc2ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

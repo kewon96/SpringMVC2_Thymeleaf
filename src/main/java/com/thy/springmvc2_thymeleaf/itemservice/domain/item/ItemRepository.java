df71ae7083b19fcb7aa14aa2c0d3294afbdbbe49
Kewon <48837736+kewon96@users.noreply.github.com>
2022-07-04 01:07:44 +0900

    체크박스 - 단일2

--- src/main/java/com/thy/springmvc2_thymeleaf/itemservice/domain/item/ItemRepository.java
+++ src/main/java/com/thy/springmvc2_thymeleaf/itemservice/domain/item/ItemRepository.java
@@ -29,9 +29,14 @@ public class ItemRepository {
 
     public void update(Long itemId, Item updateParam) {
         Item findItem = findById(itemId);
+
         findItem.setName(updateParam.getName());
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
+        findItem.setOpen(updateParam.getOpen());
+        findItem.setRegions(updateParam.getRegions());
+        findItem.setItemType(updateParam.getItemType());
+        findItem.setDeliveryCode(updateParam.getDeliveryCode());
     }
 
     public void clearStore() {
